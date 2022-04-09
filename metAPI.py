65e086af4e7c1db8abfc4dad016bf73a5ab90847
Cameron  Moy <moycam@umich.edu>
2022-04-09 18:33:03 -0400

    new database

--- metAPI.py
+++ metAPI.py
@@ -7,79 +7,49 @@ def create_database(name):
     conn = sqlite3.connect(path+f'/{name}')
     cur = conn.cursor()
 
-    # cur.execute(
-    #     """
-    #     CREATE TABLE IF NOT EXISTS Artwork
-    #     (objectID INTEGER PRIMARY KEY, isHighlight BOOLEAN, accessionYear INTEGER, imageURL STRING, title STRING, dynasty STRING, artistName STRING, artistNationality STRING, artistBirth INTEGER, artistDeath INTEGER, artistWikepedia STRING, artworkYear INTEGER, medium STRING, dimensions STRING, city STRING, state STRING, county STRING, region STRING, subregion STRING, wikepediaPage STRING)
-    #     """
-    # )
-    # conn.commit()
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS Artwork
+        (objectID INTEGER PRIMARY KEY, imageURL STRING, title STRING, cityID INTEGER, artistName STRING, artistNationality STRING, artworkYear INTEGER, medium STRING)
+        """
+    )
+    conn.commit()
 
-    # cur.execute(
-    #     """
-    #     CREATE TABLE IF NOT EXISTS Artwork_cities
-    #     (objectID INTEGER PRIMARY KEY, isHighlight BOOLEAN, accessionYear INTEGER, imageURL STRING, title STRING, dynasty STRING, artistName STRING, artistNationality STRING, artistBirth INTEGER, artistDeath INTEGER, artistWikepedia STRING, artworkYear INTEGER, medium STRING, dimensions STRING, city STRING, state STRING, county STRING, region STRING, subregion STRING, wikepediaPage STRING)
-    #     """
-    # )
-    # conn.commit()
     return cur, conn
 
-def get_API(query, cur, conn):
-    url = f"https://collectionapi.metmuseum.org/public/collection/v1/search?q={query}"
-    data = requests.get(url)
-    ids = data.json()['objectIDs']
-    
-    objects = []
-    for id in ids:
-        resp = requests.get(f'https://collectionapi.metmuseum.org/public/collection/v1/objects/{id}')
-        objects.append(resp.json())
-    
-    city_obj = []
-    city_marker = False
-    for d in objects:
-        try:
-            if d['city'] != '':
-                city_marker = True
-            for key in d.keys():
-                if d[key] == '':
-                    d[key] = 'NA'
-            if city_marker:
-                city_obj.append(d)
-            city_marker = False
-        except:
-            print('error:')
-            print(d)
+def get_API(query, city_list, cur, conn):
+    for city in city_list:   
+        city_id = city[0]
+        city_name = city[1]
 
-    for obj in objects:
-        try:
-            cur.execute(
-                """
-                INSERT OR IGNORE INTO Artwork (objectID, isHighlight, accessionYear, imageURL, title, dynasty, artistName, artistNationality, artistBirth, artistDeath, artistWikepedia, artworkYear, medium, dimensions, city, state, county, region, subregion, wikepediaPage)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """, (obj['objectID'], obj['isHighlight'], obj['accessionYear'], obj['primaryImage'], obj['title'], obj['dynasty'], obj['artistDisplayName'], obj['artistNationality'], obj['artistBeginDate'], obj['artistEndDate'], obj['artistWikidata_URL'], obj['objectDate'], obj['medium'], obj['dimensions'], obj['city'], obj['state'], obj['county'], obj['region'], obj['subregion'], obj['objectWikidata_URL'])
-            )
-            conn.commit()
-        except:
-            print("error")
-            print(obj)
-
-    for obj in city_obj:
-        try:
-            cur.execute(
-                """
-                INSERT OR IGNORE INTO Artwork_cities (objectID, isHighlight, accessionYear, imageURL, title, dynasty, artistName, artistNationality, artistBirth, artistDeath, artistWikepedia, artworkYear, medium, dimensions, city, state, county, region, subregion, wikepediaPage)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """, (obj['objectID'], obj['isHighlight'], obj['accessionYear'], obj['primaryImage'], obj['title'], obj['dynasty'], obj['artistDisplayName'], obj['artistNationality'], obj['artistBeginDate'], obj['artistEndDate'], obj['artistWikidata_URL'], obj['objectDate'], obj['medium'], obj['dimensions'], obj['city'], obj['state'], obj['county'], obj['region'], obj['subregion'], obj['objectWikidata_URL'])
-            )
-            conn.commit()
-        except:
-            print("error")
-            print(obj)
+        url = f"https://collectionapi.metmuseum.org/public/collection/v1/search?q=*&geoLocation={city_name}"
+        data = requests.get(url)
+        ids = data.json()['objectIDs']
+        
+        objects = []
+        for id in ids:
+            resp = requests.get(f'https://collectionapi.metmuseum.org/public/collection/v1/objects/{id}')
+            objects.append(resp.json())
+        
+        for obj in objects:
+            try:
+                cur.execute(
+                    """
+                    INSERT OR IGNORE INTO Artwork (objectID, imageURL, title, cityID, artistName, artistNationality, artworkYear, medium)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                    """, (obj['objectID'], obj['primaryImage'], obj['title'], city_id, obj['artistDisplayName'], obj['artistNationality'], obj['objectDate'], obj['medium'])
+                )
+                conn.commit()
+            except:
+                print("error")
+                print(obj)
         
 def main():
     cur, conn = create_database('met.db')
-    get_API('monet', cur, conn)
-    print('done')
+    artists = ['Da Vinci', 'Van Gogh', 'Rembrandt van Rijn', 'Picasso', 'Monet', 'Vermeer', 'Dali', 'Rubens', 'Michelangelo', 'Rafaël']
+    for artist in artists:
+        get_API(artist, cur, conn)
+        print('done')
 
 
 if __name__ == '__main__':
